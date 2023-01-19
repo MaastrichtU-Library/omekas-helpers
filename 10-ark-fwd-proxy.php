c458a9283dea26160bb5b9b5a75eaeed652717fb
Maarten Coonen <m.coonen@maastrichtuniversity.nl>
2023-01-19 14:43:44 +0100

    Added instructions for compatible ARK rewrite rules for .htaccess in dev,test,acc,prod servers

--- 10-ark-fwd-proxy.php
+++ 10-ark-fwd-proxy.php
@@ -11,8 +11,19 @@
 // It is catched by this 10-ark-fwd-proxy.php script and redirected to the acceptance environment based on the shoulder value 'c1'.
 
 // IMPORTANT
-// You need to add a rewrite rule like below to your Apache configuration or .htaccess file to forward incoming ARKs to this local resolver code:
+// You need the following rewrite rules and conditions in your Apache configuration or .htaccess file to forward incoming ARKs to this local resolver script:
+//
+// # Redirect incoming ARK URLs to the forward proxy code
+// RewriteCond %{REQUEST_URI} ark:/([0-9]{5})/([a-zA-Z0-9]+)$
 // RewriteRule ^ark:/([0-9]{5})/([a-zA-Z0-9]+)$ helper/10-ark-fwd-proxy.php?ark=ark:/$1/$2 [PT,L]
+// # Do not process any further rewrite rules when request URI is one of these:
+// RewriteCond %{REQUEST_URI} helper/10-ark-fwd-proxy.php [OR]
+// RewriteCond %{REQUEST_URI} helper/11-local-ark-site-resolver.php [OR]
+// RewriteCond %{REQUEST_URI} ^s/*/ark:/([0-9]{5})/([a-zA-Z0-9]+)$
+// RewriteRule .* - [L]
+//
+// # END OF ARK REWRITE RULES
+
 
 $shoulderLookup = array(
     "a1" => "http://omeka.local",
